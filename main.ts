2df8da3c07d743530c5b57da15fa57ad420325a6
da73213 <137095004+da73213@users.noreply.github.com>
2023-06-20 13:04:21 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,10 +2,6 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`gascan`, function (sprite, lo
     game.gameOver(false)
     game.setGameOverEffect(false, effects.confetti)
 })
-scene.onOverlapTile(SpriteKind.Player, sprites.castle.tilePath2, function (sprite, location) {
-    game.gameOver(true)
-    game.setGameOverEffect(true, effects.confetti)
-})
 scene.onOverlapTile(SpriteKind.Player, sprites.castle.tilePath9, function (sprite, location) {
     game.gameOver(true)
     game.setGameOverEffect(true, effects.confetti)
@@ -50,3 +46,23 @@ for (let value of tiles.getTilesByType(assets.tile`myTile`)) {
     tiles.placeOnTile(gas_can, value)
     tiles.setTileAt(value, assets.tile`transparency16`)
 }
+let myEnemy = sprites.create(img`
+    ..ccc..........ffffff...
+    ..f44c.......ffcc55ff...
+    ..f544c...fffccccfff....
+    ..f5544ccc55554445cc....
+    ..cf55cc555555555b99c...
+    .c555555555555b111999c..
+    f555ccccccc55599111bb5c.
+    fffffccc555c55555555555c
+    ...ccc55554455555555555f
+    ...c555544455555555555f.
+    ...c55544cffc5555555ff..
+    ....ccccffffcfffffff....
+    .......ffff5c5f.........
+    .......ffff5ccf.........
+    ........ffffff..........
+    ........................
+    `, SpriteKind.Enemy)
+myEnemy.follow(mySprite, 70)
+myEnemy.setPosition(0, 30)
